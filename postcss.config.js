4d90a2b402f34d2b8ea7b2a243bb384e58018093
Ladislau Ciocan <ladislau.ciocan@turistintransilvania.com>
2025-04-12 19:26:02 +0300

    Fix: updated Tailwind PostCSS config for Next.js 15

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
-/** @type {import('postcss').Config} */
 module.exports = {
   plugins: {
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
+    'postcss-nesting': {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
